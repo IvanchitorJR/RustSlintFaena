870dc77a8fb1a46c637ce93ad29a2c9eb9a94943
CarlosPerzS <carlosps2809@gmail.com>
2025-07-19 19:12:59 -0600

    conexion rust-slint punkrock

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,24 @@ pub fn android_main(app: slint::android::AndroidApp) {
     slint::android::init(app).unwrap();
 
     let ui = MainWindow::new().unwrap();
+    ui.on_login({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui_instance = ui_handle.unwrap();
+            let correo = ui_instance.get_email();
+            let pswd = ui_instance.get_pswd();
+
+            if correo.is_empty() || pswd.is_empty(){
+                ui_instance.set_empty_error("Usuario y/o contraseña incorrectos".into());
+                ui_instance.set_empty_error_text_color(ui_instance.get_rojo());
+                ui_instance.set_input_border_color(ui_instance.get_border());
+            } else {
+                ui_instance.set_empty_error("Login exitoso".into());
+                ui_instance.set_empty_error_text_color(ui_instance.get_verde());
+                ui_instance.set_input_border_color(ui_instance.get_verde());
+                println!("Login con: {} / {}", correo, pswd);
+            }
+        }
+    });
     ui.run().unwrap();
 }
