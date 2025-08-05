702a0f7ef1130b6ba26ca68494089e864f21e162
CarlosPerzS <carlosps2809@gmail.com>
2025-08-04 19:38:03 -0600

    Verificaciones para el login hechas, ahora solo falta la pantalla de inicio para finalizar todo el modulo

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 slint::include_modules!();
+use reqwest::Client;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -11,12 +12,49 @@ struct Usuario {
     premium: bool,
 }
 
+async fn verificar_credenciales(cliente: Client, correo: String, password: String, ui_handle: slint::Weak<MainWindow>){
+    let url = "https://faena-backend.onrender.com/api/usuarios"; //url para la api rest con los usuarios
+    match cliente.get(url).send().await { //enviamos la peticion y vemos las posibles respuestas
+        Ok(res) => { //si recibimos una respuesta
+            match res.json::<Vec<Usuario>>().await {
+                Ok(usuarios)=>{ //si existen usuarios o pudo la app recibir y deserializar la respuesta en un vector de usuarios(struct)
+                    let mut encontrado: bool = false;
+                    for Usuario in usuarios{
+                        if Usuario.email==correo && Usuario.contrasena==password{
+                            encontrado=true;
+                            break;
+                        }
+                    } 
+                    slint::invoke_from_event_loop(move ||{
+                        if let Some(ui) = ui_handle.upgrade(){
+                            if encontrado{
+                                ui.set_empty_error("Bienvenido a Faena".into());
+                                println!("Welcome to faena");
+                            }
+                            else{
+                                ui.set_empty_error("Usuario y/o contrasena incorrectas".into());
+                                ui.set_empty_error_text_color(ui.get_rojo());
+                                ui.set_input_border_color(ui.get_border());
+                                println!("No hay");
+                            }
+                        }
+                    }).unwrap();
+                }
+                Err(err)=>{
+                    eprintln!("Error al recibir los usuarios: {:?}", err);
+                }
+            }
+        }
+        Err(err)=>{
+                eprint!("Error al hacer la peticion, {:?}", err);
+            }
+    }
+}
 #[tokio::main]
 #[cfg(target_os = "android")]
 #[no_mangle]
 pub async fn android_main(app: slint::android::AndroidApp) {
     slint::android::init(app).unwrap();
-
     let ui = MainWindow::new().unwrap();
     let client = reqwest::Client::new(); //cliente http que provee reqwest
     //Boton de login al activarse en login.slint
@@ -24,8 +62,8 @@ pub async fn android_main(app: slint::android::AndroidApp) {
         let ui_handle = ui.as_weak(); //se hace una variable para tener la ui en el scope actual
         move || { //closure (lo que conocemos como listener)
             let ui_instance = ui_handle.unwrap(); 
-            let correo = ui_instance.get_email(); //recibimos los datos del usuario
-            let pswd = ui_instance.get_pswd();
+            let correo = ui_instance.get_email().to_string(); //recibimos los datos del usuario
+            let pswd = ui_instance.get_pswd().to_string();
 
             if correo.is_empty() || pswd.is_empty(){ //verificaciones, aca es primero esto antes del query
                 ui_instance.set_empty_error("Datos vacios".into());
@@ -34,22 +72,7 @@ pub async fn android_main(app: slint::android::AndroidApp) {
             } 
             else {
                 let client = client.clone(); //usarlo dentro de tokio::spawn
-                let url = "https://faena-backend.onrender.com/api/usuarios"; //url para la api REST de faena
-                tokio::spawn(async move{
-                    match client.get(url).send().await {
-                        Ok(res)=>{
-                            match res.json::<Vec<Usuario>>().await { 
-                                Ok(usuarios)=>{
-                                    for usuario in usuarios{
-                                        println!("{:#?}", usuario);
-                                    }
-                                }
-                                Err(err) => eprintln!("Error al deserializar usuarios: {:?}", err),
-                            }
-                        }
-                        Err(err) => eprintln!("Error al hacer request: {:?}", err),
-                    }
-                });
+                tokio::spawn(verificar_credenciales(client, correo, pswd, ui_handle.clone()));
             }
         }
     });
